eaac3d5aed643712b90a3ff922cbc2c6dbcd6e52
ikyshinicy <rranzabussines@gmail.com>
2026-05-16 15:19:57 +0900

    Implement user registration and login functions

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,51 @@
+const SUPABASE_URL = 'https://cavouyzyasnuygkuwizy.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_6eixKKot9VleMm2KVD4o7w_C58lRv6r';
 
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+async function registerUser() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const msg = document.getElementById('authMessage');
+
+  msg.innerText = 'Creating account...';
+
+  const { error } = await supabaseClient.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    msg.innerText = error.message;
+    return;
+  }
+
+  msg.innerText = 'Register success. Check your email.';
+}
+
+async function loginUser() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const msg = document.getElementById('authMessage');
+
+  msg.innerText = 'Logging in...';
+
+  const { error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    msg.innerText = error.message;
+    return;
+  }
+
+  msg.innerText = 'Login success';
+
+  setTimeout(() => {
+    window.location.href = '../dashboard/index.html';
+  }, 1000);
+}
